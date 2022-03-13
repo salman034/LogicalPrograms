aa55db35e0e73e711f0ebe26f58a33ec5bc653f3
unknown <you@example.com>
2022-03-14 01:59:04 +0530

    Added: Problem Based On logical programs

---
+++ src/logicalproblems/FibonacciSeries.java
@@ -0,0 +1,9 @@
+package logicalproblems;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Logical problems");
+    }
+}
