b215955ca00a691068f091a62a0367aa9f3c7885
unknown <you@example.com>
2022-03-14 02:04:19 +0530

    Added: Finding out the Fibonacci series

--- src/logicalproblems/FibonacciSeries.java
+++ src/logicalproblems/FibonacciSeries.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Logical problems");
+        Scanner scanner = new Scanner(System.in);
+        int Number1 = 0, Number2 =1;
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+        System.out.println("First " + number + " terms of fibonnci series ");
+        scanner.close();
+
+        for (int i = 1; i <= number; ++i)
+        {
+            System.out.println(Number1 + " ");
+            int Sum = Number1 + Number2;
+            Number1 = Number2;
+            Number2 = Sum;
+        }
     }
 }
